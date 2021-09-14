8a3c3b7119b091ce184f473230b8c4b7276c7412
leudy.delossantos <leudy.delossantos@pgr.gob.do>
2021-09-14 15:19:32 -0400

    Iniciando proyecto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombres { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+        [Required]
+        public string Direccion { get; set; }
+        [Required]
+        public string Telefono { get; set; }
+    }
+}
